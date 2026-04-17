1bea5046647c1d7159578b796bd39e44977ea93a
Gavi Narra <gavi.narra@objectgraph.com>
2026-04-17 10:20:32 -0400

    sync homebrew formula scaffold with shipped 0.1.0

--- packaging/homebrew-tap/Formula/monmon.rb
+++ packaging/homebrew-tap/Formula/monmon.rb
@@ -3,29 +3,65 @@ class Monmon < Formula
 
   desc "macOS silicon monitor (E/P cores, GPU, NPU) with a TUI"
   homepage "https://github.com/gavi/monmon"
-  # After `uv build && uv publish`, replace the url + sha256 with the PyPI sdist.
-  # Example:
-  #   url "https://files.pythonhosted.org/packages/source/m/monmon/monmon-0.1.0.tar.gz"
-  # You can find the URL + sha256 on the PyPI project page under "Download files".
-  url "https://files.pythonhosted.org/packages/source/m/monmon/monmon-0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_SDIST_SHA256"
+  url "https://files.pythonhosted.org/packages/f7/34/b3e0e4c7e7916534910e0ed92cb2307732009bdd198e1a0fcfe74c56e860/monmon-0.1.0.tar.gz"
+  sha256 "c516b00d131b38a6cd054cd58b5190daf6b96071a7b70ff9dc4d76588d089a9f"
   license "MIT"
 
   depends_on "python@3.12"
 
-  # The `resource` stanzas below are placeholders. After editing this formula,
-  # run:
-  #   brew update-python-resources monmon
-  # It will rewrite these blocks with real PyPI URLs + sha256s for every
-  # transitive dependency of monmon.
+  resource "linkify-it-py" do
+    url "https://files.pythonhosted.org/packages/2e/c9/06ea13676ef354f0af6169587ae292d3e2406e212876a413bf9eece4eb23/linkify_it_py-2.1.0.tar.gz"
+    sha256 "43360231720999c10e9328dc3691160e27a718e280673d444c38d7d3aaa3b98b"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
+  end
+
+  resource "mdit-py-plugins" do
+    url "https://files.pythonhosted.org/packages/b2/fd/a756d36c0bfba5f6e39a1cdbdbfdd448dc02692467d83816dff4592a1ebc/mdit_py_plugins-0.5.0.tar.gz"
+    sha256 "f4918cb50119f50446560513a8e311d574ff6aaed72606ddae6d35716fe809c6"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/9f/4a/0883b8e3802965322523f0b200ecf33d31f10991d0401162f4b23c698b42/platformdirs-4.9.6.tar.gz"
+    sha256 "3bfa75b0ad0db84096ae777218481852c0ebc6c727b3168c1b9e0118e458cf0a"
+  end
+
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/source/p/psutil/psutil-7.2.2.tar.gz"
-    sha256 "REPLACE"
+    url "https://files.pythonhosted.org/packages/aa/c6/d1ddf4abb55e93cebc4f2ed8b5d6dbad109ecb8d63748dd2b20ab5e57ebe/psutil-7.2.2.tar.gz"
+    sha256 "0746f5f8d406af344fd547f1c8daa5f5c33dbc293bb8d6a16d80b4bb88f59372"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/c0/8f/0722ca900cc807c13a6a0c696dacf35430f72e0ec571c4275d2371fca3e9/rich-15.0.0.tar.gz"
+    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
   end
 
   resource "textual" do
-    url "https://files.pythonhosted.org/packages/source/t/textual/textual-8.2.3.tar.gz"
-    sha256 "REPLACE"
+    url "https://files.pythonhosted.org/packages/cf/2f/d44f0f12b3ddb1f0b88f7775652e99c6b5a43fd733badf4ce064bdbfef4a/textual-8.2.3.tar.gz"
+    sha256 "beea7b86b03b03558a2224f0cc35252e60ef8b0c4353b117b2f40972902d976a"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
+  end
+
+  resource "uc-micro-py" do
+    url "https://files.pythonhosted.org/packages/78/67/9a363818028526e2d4579334460df777115bdec1bb77c08f9db88f6389f2/uc_micro_py-2.0.0.tar.gz"
+    sha256 "c53691e495c8db60e16ffc4861a35469b0ba0821fe409a8a7a0a71864d33a811"
   end
 
   def install
